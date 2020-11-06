c471747540abc308c330ffb5ceeee519b5f8f293
ntorresm <odon.niel.torres.mariluz@everis.com>
2020-11-06 14:20:22 +0100

    feat: Añado paquete final de portafolio web nieltorres.com con animaciones con sass

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,7 @@
 const ipad = window.matchMedia('screen and (max-width: 767px)');
 const menu = document.querySelector('.menu');
 const burgerButton = document.querySelector('#burger-menu');
+const headerlogo = document.querySelector('#logo');
 
 if(ipad.addEventListener) {// todos los principales navegadores, excepto IE 8 y versiones 
     // console.log("::addEventListener::");
@@ -31,31 +32,52 @@ function hideShowMenu(element){
     if (burgerButton.classList.contains('on')) {
         burgerButton.classList.remove('on');
         menu.classList.remove('is_active');
+        // headerlogo.classList.add('hide');
     } else {
         burgerButton.classList.add('on');
         menu.classList.add('is_active');
+        // headerlogo.classList.remove('hide');
     }
 }
 
+$(window).on('beforeunload', function() {
+
+    window.setTimeout(function() {
+        $(window).scrollTop(0); 
+    }, 0);
+ 
+});
+ 
+
 $(window).scroll(function(event) {
+    
     const header = document.querySelector('.header');
+    var $pos = 5;
     // var scrollLeft = $(window).scrollLeft();
     var scrollTop = $(window).scrollTop();
     // console.log("Vertical "+scrollTop);
     // console.log("Horizontal "+scrollLeft);
-    
-    if(scrollTop>5){
-        header.classList.add("sticky");
-                
+
+    if(scrollTop>$pos){
+        header.classList.add("sticky");        
     }
     else {
         header.classList.remove("sticky");
     }
-  }); 
+
+    // console.log('SI, estas haciendo scroll');
+    // clearTimeout( $.data( this, "TestScroll" ) );
+    // $.data( this, "TestScroll", setTimeout(function() {
+    //     console.log('NO estas haciendo scroll');
+    // }, 250) );
+    
+}); 
 
 function sendEmailContact(){
-    const email = $("#email").val();
-    window.open('mailto:'+email+'?subject=subject&body=body');
+    const email = "niel.torres08@gmail.com";
+    var subject = "Hola Niel, ponte en contacto conmigo cuando puedas";
+    var body = "";
+    window.open('mailto:'+email+'?subject='+subject+'&body='+body+'');
 }  
 
 
