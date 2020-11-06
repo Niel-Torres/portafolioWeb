635ad412ce3a52e9d8d9a47990fa247eda20d28c
ntorresm <odon.niel.torres.mariluz@everis.com>
2020-11-06 20:05:35 +0100

    feat: Añado enlace scrolltop para ir al top de la web, tiene funcionalidad js para mostrarlo o ocultarlo de forma dinámica

--- js/myscript.js
+++ js/myscript.js
@@ -2,6 +2,8 @@ const ipad = window.matchMedia('screen and (max-width: 767px)');
 const menu = document.querySelector('.menu');
 const burgerButton = document.querySelector('#burger-menu');
 const headerlogo = document.querySelector('#logo');
+const header = document.querySelector('.header');
+const scrollArrow = document.querySelector('.scrollToTop');
 
 if(ipad.addEventListener) {// todos los principales navegadores, excepto IE 8 y versiones 
     // console.log("::addEventListener::");
@@ -10,6 +12,10 @@ if(ipad.addEventListener) {// todos los principales navegadores, excepto IE 8 y 
     // console.log("::attachEvent::");
     ipad.attachEvent("onchange", validationMatchMedia);
 }
+
+/*=======================================================================================================*/
+// Comprueba si estamos a resolución < 768, si es así empieza a escuchar event click
+/*=======================================================================================================*/
 
 validationMatchMedia();
 
@@ -40,6 +46,44 @@ function hideShowMenu(element){
     }
 }
 
+
+// if (ipad.matches) {
+//     // console.log("::TRUE::");
+//     burgerButton.addEventListener('click', hideShow);
+// } else {
+//     // console.log("::FALSE::");
+//     burgerButton.removeEventListener('click', hideShow);
+// }
+
+function hideShow(e) {
+    e.preventDefault();
+    hideShowMenu();
+}
+
+/*=======================================================================================================*/
+// Función para ocultar/mostrar el menu y darle movimiento al burgerMenu, ambas usando classes css
+/*=======================================================================================================*/
+function hideShowMenu(element){
+    if (burgerButton.classList.contains('on')) {
+        burgerButton.classList.remove('on');
+        menu.classList.remove('is_active');
+        // headerlogo.classList.add('hide');
+    } else {
+        burgerButton.classList.add('on');
+        menu.classList.add('is_active');
+        // headerlogo.classList.remove('hide');
+    }
+}
+
+//Click event to scroll to top
+$('.scrollToTop').click(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+    return false;
+});
+
+//Click event to load page to top    
 $(window).on('beforeunload', function() {
 
     window.setTimeout(function() {
@@ -49,9 +93,10 @@ $(window).on('beforeunload', function() {
 });
  
 
+/*=======================================================================================================*/
+// Función para comprobar el scrollTop para interactuar con el header y la flecha scrollTop
+/*=======================================================================================================*/
 $(window).scroll(function(event) {
-    
-    const header = document.querySelector('.header');
     var $pos = 5;
     // var scrollLeft = $(window).scrollLeft();
     var scrollTop = $(window).scrollTop();
@@ -59,10 +104,12 @@ $(window).scroll(function(event) {
     // console.log("Horizontal "+scrollLeft);
 
     if(scrollTop>$pos){
-        header.classList.add("sticky");        
+        header.classList.add("sticky");
+        scrollArrow.classList.remove("hide");
     }
     else {
         header.classList.remove("sticky");
+        scrollArrow.classList.add("hide");
     }
 
     // console.log('SI, estas haciendo scroll');
@@ -73,15 +120,13 @@ $(window).scroll(function(event) {
     
 }); 
 
+
+/*=======================================================================================================*/
+// Función para enviar email
+/*=======================================================================================================*/
 function sendEmailContact(){
     const email = "niel.torres08@gmail.com";
     var subject = "Hola Niel, ponte en contacto conmigo cuando puedas";
     var body = "";
     window.open('mailto:'+email+'?subject='+subject+'&body='+body+'');
 }  
-
-
-
-
-
-
